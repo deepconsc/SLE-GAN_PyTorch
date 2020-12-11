aa387851ad7315144b68f611f3ca6748c1edec92
aleksi_kistauri <lchkhetiani@systemcorp.ai>
2020-12-11 20:37:36 +0400

    trainer

---
+++ utils/train.py
@@ -0,0 +1,59 @@
+from models.generator import Generator 
+from models.discriminator import Discriminator
+import torch 
+from torch import nn 
+from torch.nn import functional as F 
+from torch.distributions import normal
+from random import randint 
+
+
+N = normal.Normal(torch.tensor([0.0]), torch.tensor([1.0])) # Initializing Normal distribution sampler
+
+def trainer(generator, discriminator, optim_g, optim_d, losses, trainloader, device, log_interval):
+    for epoch in range(1, n_epochs+1):
+        loader = tqdm.tqdm(trainloader, desc='Loading train data')
+        randomspace = [randint(4,12) for i in range(len(trainloader))]  # Generating random numbers for random crop on the fly, instead of overengineering dataloader & discriminator
+        for iter, batch in enumerate(loader):
+            randn = randomspace[iter] # Random by iter
+            img_L, img_D = batch.to(device)
+            noise = N.sample([img_L.shape[0], 1, 1, 256]).squeeze(-1).to(torch.device(device)) # Sample normal distribution noise
+
+            # Generator 
+
+            optim_g.zero_grad()
+            fake_images = generator(noise)
+            fake_L = fake_images.detach() 
+
+            fake_logits, fake_absolute, fake_randcrop = model_d(fake_images, randn) 
+            fake_logits_G = fake_logits.detach()
+
+            gen_loss = losses.generator_loss(fake_logits_G)
+            gen_loss.backward()
+            optim_g.step()
+
+
+            # Discriminator 
+
+            real_logits, real_absolute, real_randcrop = model_d(img_D, randn)
+            reconst_loss_absolute = losses.reconstruction_loss(img_L, real_absolute) # Absolute image reconstruction loss
+            reconst_loss_randcrop = losses.reconstruction_loss(img_L[:,:,randn-4:randn+4,randn-4:randn+4], real_randcrop) # Random cropped image reconstruction loss
+            disc_loss = losses.disc_loss(real_logits, fake_logits)
+            
+            disc_total_loss = reconst_loss_absolute + reconst_loss_randcrop + disc_loss
+
+            disc_total_loss.backward()
+            optim_d.step()
+
+
+            if iter % log_interval == 0:
+                loader.set_description(f"Epoch: {epoch} | G Loss: {gen_loss.item()} | D Loss: {disc_total_loss.item()} | Step: {iter}")
+        
+        # Generating samples at the end of the epoch
+
+        with torch.no_grad():
+            noise = N.sample([img_L.shape[0], 1, 1, 256]).squeeze(-1).to(torch.device(device))
+            images = generator(noise).detach().cpu()
+            img_array = [img.squeeze(0).transpose(2,0,1).numpy() for img in images]
+            
+
+
