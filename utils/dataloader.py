7f38d9f211e844c1e7b004ec1ea545e939463d3d
aleksi_kistauri <lchkhetiani@systemcorp.ai>
2020-12-13 21:06:29 +0400

    dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -17,8 +17,8 @@ class DataSampler(Dataset):
         image_name = self.input_images[idx]
         image = cv2.imread(image_name)
         image = cv2.resize(image, (self.size, self.size))
-        image = torch.from_numpy(image).transpose(2,0,1)
-        image = (image - 127.5) / 127.5
+        image = torch.from_numpy(image)
+        image = (image.transpose(2,0,1) - 127.5) / 127.5
 
         return [image, image.clone()]
     
