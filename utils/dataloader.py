da176f0fa767a2daa3ee5947181ba30008375000
aleksi_kistauri <lchkhetiani@systemcorp.ai>
2020-12-13 20:47:01 +0400

    dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -4,17 +4,18 @@ import glob
 from torch.utils.data import Dataset, DataLoader
 
 class DataSampler(Dataset):
-    def __init__(self, path, batch_size, threads):
+    def __init__(self, path, batch_size, threads, resolution):
         self.input_images = glob.glob(f'{path}/*') 
         self.bs = batch_size 
         self.threads = threads 
-
+        self.size = resolution
     def __len__(self):
         return len(self.input_images)
     
     def __getitem__(self, idx):        
         image_name = self.input_images[idx]
         image = cv2.imread(image_name)
+        image = cv2.resize(image, (size, size))
         image = torch.from_numpy(image, dtype=torch.float).transpose(2,0,1)
         image = (image - 127.5) / 127.5
 
