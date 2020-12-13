3ae01dbd94dc72a23320f7965261f9b25907ba58
aleksi_kistauri <lchkhetiani@systemcorp.ai>
2020-12-13 21:17:14 +0400

    dataloader

--- utils/loss.py
+++ utils/loss.py
@@ -1,14 +1,15 @@
 import torch.nn as nn 
 import torch
 import torch.nn.functional as F
-
+from torch.autograd import Variable
 
 class Loss(nn.Module):
     def __init__(self):
         super(Loss, self).__init__()
     
     def reconstruction_loss(real_image, fake_image):
-        return torch.mean(F.l1_loss(real_image, fake_image))
+        reconst = torch.mean(F.l1_loss(real_image, fake_image))
+        return Variable(reconst, requires_grad=True)
 
 
     def disc_loss(real_logits, fake_logits):
@@ -17,9 +18,10 @@ class Loss(nn.Module):
 
         fake_loss = torch.min(0.0, -1 - fake_logits)
         fake_loss = -1 * torch.mean(fake_loss)
-
-        return real_loss + fake_loss
+        totalloss = real_loss + fake_loss
+        return Variable(totalloss, requires_grad=True)
 
 
     def generator_loss(fake_logits):
-        return -1 * torch.mean(fake_logits)
+        gen_loss = -1 * torch.mean(fake_logits)
+        return Variable(gen_loss, requires_grad=True)
