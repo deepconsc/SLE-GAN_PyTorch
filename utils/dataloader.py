2ddd40df2f441a4524f5f070467e69a6523111e6
aleksi_kistauri <lchkhetiani@systemcorp.ai>
2020-12-13 21:33:08 +0400

    dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -15,10 +15,18 @@ class DataSampler(Dataset):
     
     def __getitem__(self, idx):        
         image_name = self.input_images[idx]
-        image = cv2.imread(image_name)
-        image = cv2.resize(image, (self.size, self.size))
-        image = torch.from_numpy(image)
-        image = (image.permute(2,0,1) - 127.5) / 127.5
+        try:
+            image = cv2.imread(image_name)
+            image = cv2.resize(image, (self.size, self.size))
+            image = torch.from_numpy(image)
+            image = (image.permute(2,0,1) - 127.5) / 127.5
+        except cv2.error:
+            image_name = self.input_images[idx+1]
+            image = cv2.imread(image_name)
+            image = cv2.resize(image, (self.size, self.size))
+            image = torch.from_numpy(image)
+            image = (image.permute(2,0,1) - 127.5) / 127.5
+
 
         return [image, image.clone()]
     
