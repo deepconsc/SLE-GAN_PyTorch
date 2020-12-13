1268d0a292c5e347408f72f3f3c825d4f519554a
aleksi_kistauri <lchkhetiani@systemcorp.ai>
2020-12-14 00:00:24 +0400

    dataloader

--- utils/train.py
+++ utils/train.py
@@ -43,6 +43,7 @@ def trainer(generator, discriminator, optim_g, optim_d, trainloader, n_epochs, d
 
             real_logits, real_absolute, real_randcrop = discriminator(img_D, randn)
             reconst_loss_real = Variable(F.relu(torch.rand_like(real_logits) * 0.2 + 0.8 - real_logits).mean() + perceptual(F.interpolate(img_L, size=(128,128)), real_absolute) + perceptual(F.interpolate(img_L[:,:,xy0:xy1,xy0:xy1], size=(128,128)), real_randcrop), requires_grad=True)
+            print(reconst_loss_real)
             #reconst_loss_absolute = Loss.reconstruction_loss(F.interpolate(img_L, size=(128,128)).cpu(), real_absolute.cpu()) # Absolute image reconstruction loss
             reconst_loss_fake = Variable(F.relu(torch.rand_like(real_logits) * 0.2 + 0.8 + real_logits).mean(), requires_grad=True)
             # Calculate random crop proportions in prior
